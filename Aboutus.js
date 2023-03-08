f0be891ad3d600bcbac9c1163621c56bb9346e24
saicharan <saicharan_b200970ec@nitc.ac.in>
2023-03-08 23:34:26 +0530

    upgraded hidemenu

--- Aboutus.js
+++ Aboutus.js
@@ -1,6 +1,11 @@
 
+function none() {
+    if (document.getElementById('sidemenu').style.display == 'block') {
+        myFunction(document.getElementsByClassName('hidemenu')[0]);
+    }
+}
 function myFunction(x) {
-    
+
     x.classList.toggle("change");
 
     if (document.getElementById('sidemenu').style.display == 'block') {
